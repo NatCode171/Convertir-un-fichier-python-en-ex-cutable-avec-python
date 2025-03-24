c7d9b6e3a5f8c0130bb89350f0ea4af44dcceecb
NatCode <NatCode171@gmail.com>
2025-03-24 19:46:32 +0100

    Update main.py
    Amélioration du script:
     - Un traitement des erreurs.
     - Un moyen de cacher le cmd !

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 # pip install nuitka
 
-import os
-import subprocess
+import os, subprocess, sys
 
-# Vous pourrez enlever ces print() ;)
-print()
-print("Programme fait par NatCode (@NatCode171 sur youtube)")
+print("Programme fait par NatCode (@NatCode sur youtube)")
 print()
 print("Mes sites :")
 print("   - https://natcode.fr.to/")
@@ -23,27 +20,48 @@ print("*************************************************************************
 print()
 print("Exemple d'utilisation :")
 print()
-print("**************************************************************************")
-print("*  Entrez l'emplacement du fichier : D:\Programmes\Virus_Firefox         *")
-print("*  Entrez le nom du fichier (avec l'extension) : main.py                 *")
-print("*  Le programme devrai s'éxécuter :)                                     *")
-print("**************************************************************************")
-print()
+print("****************************************************************************************")
+print("*  Entrez l'emplacement complet du fichier (avec l'extension) : D:\Programmes\main.py  *")
+print("*  Entrez le Voulez-vous que le cmd sois masqué ? [yes/no] : yes                       *")
+print("*  -- Le programme devrai s'éxécuter :) --                                             *")
+print("****************************************************************************************\n")
 
-# Demander à l'utilisateur d'entrer l'emplacement du fichier
-emplacement = input(" Entrez l'emplacement du fichier : ")
+def main():
+    # Demander à l'utilisateur d'entrer l'emplacement complet du fichier
+    emplacement_complet = input("Entrez l'emplacement complet du fichier (avec l'extension) : ")
 
-# Demander à l'utilisateur d'entrer le nom du fichier avec l'extension
-nom = input(" Entrez le nom du fichier (avec l'extension) : ")
+    # Extraire le nom du fichier (avec l'extension) et son emplacement
+    emplacement, nom = os.path.split(emplacement_complet)
 
-# Vérifier si le fichier existe
-chemin_complet = os.path.join(emplacement, nom)
-if not os.path.exists(chemin_complet):
-    print("Erreur: Le fichier spécifié n'existe pas.")
-    exit(1)
+    # Vérifier si le fichier existe
+    if not os.path.exists(emplacement_complet):
+        print("Erreur: Le fichier spécifié n'existe pas.")
+        exit(1)
 
-# Changer de répertoire vers l'emplacement spécifié
-os.chdir(emplacement)
+    # Changer de répertoire vers l'emplacement spécifié
+    os.chdir(emplacement)
 
-# Exécuter nuitka pour compiler le fichier spécifié en un seul fichier exécutable
-subprocess.run(f'py -m nuitka --onefile --standalone {nom}', shell=True)
+    # Exécuter nuitka pour compiler le fichier spécifié en un seul fichier exécutable
+    choix_cmd = input("Voulez-vous que le cmd sois masqué ? yes/[no] : ").lower()
+    if choix_cmd == "yes" or choix_cmd == "y":
+        commande = f'"{sys.executable}" -m nuitka --onefile --standalone --windows-disable-console {nom}'
+    else:
+        commande = f'"{sys.executable}" -m nuitka --onefile --standalone {nom}'
+
+    # On exécute la commande et traite les erreurs ;)
+    try:
+        subprocess.run(commande, shell=True)
+    except Exception as e:
+        print(f"Une erreur est survenue :\n{e}")
+        print(f'Voici la commande qui a échoué : {commande}')
+        retry = input("Voulez-vous modifier la commande ? yes/[no] : ").lower()
+        if retry == "yes" or retry == "y":
+            commande2 = input("Votre commande : ")
+            try:
+                subprocess.run(commande2, shell=True)
+            except Exception as e:
+                print(f"Une erreur est survenue, annulation...\nErreur :\n{e}\n")
+        else:
+            print("Anulation de la commande...\n")
+
+main()
